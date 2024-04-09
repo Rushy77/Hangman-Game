03dd063ac9d4a0710df51d390e6217d827887a86
Rushy77 <165278379+Rushy77@users.noreply.github.com>
2024-04-09 12:45:58 +0100

    Update difficultypage.js

--- Hangman-game-project/link/difficultypage.js
+++ Hangman-game-project/link/difficultypage.js
@@ -1,10 +1,30 @@
 const backarrow = document.querySelector('.back-arrow');
-const beginnerpg = document.querySelector('#beginner-btn');
+const beginnerbtn = document.querySelector('#beginner-btn');
+const amateurbtn = document.querySelector('#amateur-btn');
+const intermediatebtn = document.querySelector('#intermediate-btn');
+const probtn = document.querySelector('#pro-btn');
+const expertbtn = document.querySelector('#expert-btn');
 
 backarrow.addEventListener('click', function(){
     window.location='../index.html';
 });
 
-beginnerpg.addEventListener('click', function(){
+beginnerbtn.addEventListener('click', function(){
     window.location='difficulties/beginnerpage.html';
+});
+
+amateurbtn.addEventListener('click', function(){
+    window.location='difficulties/amateurpage.html';
+});
+
+intermediatebtn.addEventListener('click', function(){
+    window.location='difficulties/intermediatepage.html';
+})
+
+probtn.addEventListener('click', function(){
+    window.location='difficulties/propage.html';
+});
+
+expertbtn.addEventListener('click', function(){
+    window.location='difficulties/expertpage.html';
 });
